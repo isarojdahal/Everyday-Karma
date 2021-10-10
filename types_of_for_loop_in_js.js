ce8ba3c059beff56d8eaee21c4fe4a773055b4cc
Saroj Dahal <sarojdahal1213@gmail.com>
2021-10-10 09:30:01 +0545

    added: types_of_for_loop

---
+++ types_of_for_loop_in_js.js
@@ -0,0 +1,19 @@
+// for in
+
+var fruits = ["Apple", "Mango"];
+// var fruits = { 20: "Apple", 80: "Mango" };
+
+// for (var i in fruits) {
+//   console.log(i);
+// }
+
+//for of loop
+
+// for (var i of fruits) {
+//   console.log(i);
+// }
+
+// for each loop
+fruits.forEach((value, index) => {
+  console.log(index);
+});
