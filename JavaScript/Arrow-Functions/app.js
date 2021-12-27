87b516bfdd70ea51429a825e43729489ac55a06d
isarojdahal <sarojdahal@Sarojs-MacBook-Pro.local>
2021-12-27 21:44:42 +0545

    added: arrow-function and template-literal code

---
+++ JavaScript/Arrow-Functions/app.js
@@ -0,0 +1,17 @@
+// function abc (a,b){
+
+//     return "hello";
+// }
+
+// const abcFunction = function(){
+
+//     return "hello";
+// }
+
+
+const abcFunction = (a,b)=>a+b;
+
+
+
+
+console.log(abcFunction(1,2))
---
+++ JavaScript/Template Literals/app.js
@@ -0,0 +1,28 @@
+var firstName = "Saroj";
+
+// console.log(`My first name is : ${firstName}`)
+
+// console.log(`this is random 
+
+
+// text 
+
+
+// `);
+
+// console.log("a\"b")
+// 'this create\'s '
+
+// console.log(` a" b  ; askfjd' `);
+
+
+// function abc(){
+
+//     return "hello";
+// }
+// console.log(`${abc()}`);
+
+
+var age = 23;
+
+console.log(`${age>18?"Can vote":"Can't vote"}`);
