7b6210f8ca6adab7e71d591409e8cedb308e01fd
isarojdahal <sarojdahal@sdsys.local>
2022-01-02 18:01:49 +0545

    added: ES6 Modules dir

---
+++ JavaScript/ES6 Modules/app.js
@@ -0,0 +1,29 @@
+export default function add(a,b){
+    return a+b;
+}
+
+//  class Student{
+
+//     constructor(name){
+
+//         this.name = name;
+//     }
+
+//     getStudentInfo(){
+
+//         console.log(`Hello ${this.name}`)
+//     }
+// }
+
+// const studentList = ["ShreeKrishna","Abiral"];
+
+// const API_KEY = "aasjfasfj";
+
+// const user = {
+
+//     id:1,
+//     name:"ABC"
+// };
+
+// export {add, Student, studentList, user, API_KEY};
+
---
+++ JavaScript/ES6 Modules/main.js
@@ -0,0 +1,27 @@
+
+// import add from "./app.js"
+
+// console.log(add(1,6))
+
+import React,{useState, useEffect} from "react";
+// import {useState, useEffect } from "react";
+
+
+
+// import {add as myAdder , Student as MyStudent, studentList, user} from "./app.js"
+
+
+
+// console.log(myAdder(1,5))
+
+
+// // import {Student} from "./app.js";
+// import {studentList} from "./app.js"
+// import {user} from "./app.js"
+
+// console.log(add(1,2));
+
+// var student = new Student("Saroj");
+// student.getStudentInfo();
+
+// console.log(studentList);
